fbb66a75d4ea63e38b6342c759649efd03b40892
MAXIM <ehzine@gmail.com>
2020-05-19 01:02:24 +0300

    Basic window

--- src/crawler/WebCrawler.java
+++ src/crawler/WebCrawler.java
@@ -1,11 +1,34 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        setTitle("Web crawler");
+        var panel = initPanel();
+        add(panel);
         setVisible(true);
+    }
+
+    private JPanel initPanel() {
+        JPanel listPane = new JPanel();
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+        var textArea = initTextArea();
+
+        listPane.add(textArea);
+        listPane.add(Box.createRigidArea(new Dimension(50 , 10)));
+        listPane.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        return listPane;
+    }
+
+    private JTextArea initTextArea() {
+        JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");
+        textArea.setText("HTML code?");
+        textArea.setEnabled(false);
+        return textArea;
     }
 }
